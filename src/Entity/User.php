451dd382c412e3f453440f09ec5815ba4b96b74c
olga <olgafff@free.fr>
2022-02-10 19:50:05 +0100

    Add customers:read, Customer list for User

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,12 +2,13 @@
 
 namespace App\Entity;
 
-use App\Repository\UserRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use App\Repository\UserRepository;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
@@ -16,7 +17,12 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
+    #[Groups("customers:read")]
     private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    #[Groups("customers:read")]
+    private $name;
 
     #[ORM\Column(type: 'string', length: 180, unique: true)]
     private $email;
@@ -34,10 +40,8 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private $products;
 
     #[ORM\OneToMany(mappedBy: 'client', targetEntity: Customer::class)]
+    #[Groups("customers:read")]
     private $customers;
-
-    #[ORM\Column(type: 'string', length: 255)]
-    private $name;
 
     public function __construct()
     {
